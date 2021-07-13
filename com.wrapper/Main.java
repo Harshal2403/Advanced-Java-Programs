7a07d1018ac553154e716a78d799d173f08a9f78
Harshal2403 <56926517+Harshal2403@users.noreply.github.com>
2021-07-13 11:12:51 -0700

    Add files via upload

---
+++ com.wrapper/Main.java
@@ -0,0 +1,15 @@
+package com.wrapper;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    int a = 10;
+
+    Integer i = new Integer(a);
+
+    System.out.println("Autoboxing: ");
+    System.out.println("int     a = "+ a);
+    System.out.println("Integer i = "+i);
+    }
+}
