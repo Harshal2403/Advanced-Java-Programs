a5e5a85e6dfac71736221be8128947109fee3f54
Harshal2403 <56926517+Harshal2403@users.noreply.github.com>
2021-07-14 06:09:00 -0700

    Add files via upload

---
+++ com.wrapper/wrapperTest3.java
@@ -0,0 +1,16 @@
+package com.wrapper;
+
+public class wrapperTest3 {
+    public static void main(String[] args) {
+        int x=35,z=35;
+        Integer y = new Integer(35);
+        Integer w = new Integer(35);
+
+        if (y==w) {
+            System.out.println("hhhi");
+        }
+        else {
+            System.out.println("Hello!");
+        }
+    }
+}
